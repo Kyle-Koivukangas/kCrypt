8f91b435dbe2baa14e62ef0462c3e19e3e2495a0
Kyle Koivukangas <kyle.koivukangas@gmail.com>
2016-12-13 22:56:26 -0700

    Formatting & variable renames.
    Did some formatting and changed single letter variable names to hopefully make it more readable. Also added a keys variable when kCrypt is called for debugging purposes as I work on multithreading.

--- kCrypt.py
+++ kCrypt.py
@@ -1,5 +1,4 @@
-"""
-K-Crypt encryption module, by Kyle Koivukangas
+"""K-Crypt encryption module, by Kyle Koivukangas
 just messing around in Python, thought RSA encryption would be a fun little challenge.
 I'm basically just following this https://simple.wikipedia.org/wiki/RSA_(algorithm)#Encrypting_messages
 
@@ -15,6 +14,7 @@ import textConvert
 
 _encrypt_chunk_size = 1
 
+
 def full_test():
     """series of prints and function calls to test kCrypt as a whole    """
 
@@ -27,7 +27,7 @@ def full_test():
     encrypted_sequence = split_encrypt(numeral_sequence, _encrypt_chunk_size)
     print "encrypted_sequence: %s" % encrypted_sequence
 
-    decrypted_sequence = split_decrypt(encrypted_sequence, _encrypt_chunk_size)
+    decrypted_sequence = split_decrypt(encrypted_sequence)
     print "decrypted_numeral_sequence: %s" % decrypted_sequence
 
     decrypted_sequence = textConvert.convert_to_alpha(decrypted_sequence)
@@ -36,27 +36,33 @@ def full_test():
 
 def generate_keys(prime_range_start, prime_range_stop):
     """generates RSA keys as tuples the original primes used to generate the keys are generated from a range between the given parameters"""
-    p, q = random_prime(prime_range_start, prime_range_stop)
-    n = (p - 1) * (q - 1)
-    e = find_public_exponent(n)
-    d = find_private_exponent(n, e)
-    public_key = (p*q, e)
-    private_key = (p*q, d)
+    prime1, prime2 = random_prime(prime_range_start, prime_range_stop)
+    modulus = prime1 * prime2
+    totient = (prime1 - 1) * (prime2 - 1)
+    public_exponent = find_public_exponent(totient)
+    private_exponent = find_private_exponent(totient, public_exponent)
+    public_key = (modulus, public_exponent)
+    private_key = (modulus, private_exponent)
     return (public_key, private_key)
+
 
 def random_prime(start, stop):
     """generates a tuple of two random prime numbers within the range of the given parameters (start, stop)"""
     list_of_primes = prime_list(start, stop)
-    #random_indice = random.choice(list_of_primes) #subtract extra to balance next lines addition so you don't go out of range
+    # random_indice = random.choice(list_of_primes) #subtract extra to balance
+    # next lines addition so you don't go out of range
+
     return random.choice(list_of_primes), random.choice(list_of_primes)
 
-def prime_list(start, stop): 
-    """Generates a list of prime numbers between the two given parameters (start, stop)"""    
-    if stop == 2: 
-    	return [2]
-    elif start < 3: 
-    	start = 3
+
+def prime_list(start, stop):
+    """Generates a list of prime numbers between the two given parameters (start, stop)"""
+    if stop == 2:
+        return [2]
+    elif start < 3:
+        start = 3
     return [x for x in range(start, stop) if is_prime(x)]
+
 
 def is_prime(number):
     """tests whether a number is prime or not"""
@@ -66,18 +72,23 @@ def is_prime(number):
     return True
 
 
-def find_public_exponent(n):
+def find_public_exponent(totient):
     """calculates the public key exponent ('e') for the RSA encryption algorithm"""
-    list_of_coprimes = coprime_list(n)
-    return list_of_coprimes[random.randint(0, 50)] #first 50 indices of coprimes
+    list_of_coprimes = coprime_list(totient)
+    # chooses from the first 50 indices of coprimes to keep the numbers
+    # relatively close
+    return list_of_coprimes[random.randint(0, 50)]
 
-def coprime_list(n):
+
+def coprime_list(totient):
     """returns a list of coprimes of a number"""
-    return [x for x in range(3, n, 2) if is_coprime(x, n)]
+    return [x for x in range(3, totient, 2) if is_coprime(x, totient)]
+
 
 def is_coprime(a, b):
-    """tests whether two numbers are coprime or not"""
+    """tests whether two numbers are coprime or not (coprime if GCD == 1)"""
     return greatest_common_denominator(a, b) == 1
+
 
 def greatest_common_denominator(a, b):
     """returns the greatest common denominator of the two given numbers"""
@@ -89,43 +100,43 @@ def greatest_common_denominator(a, b):
     return max(a, b)
 
 
-def find_private_exponent(n, e):
+def find_private_exponent(totient, public_exponent):
     """calculates the private key exponent ('d') for the RSA encryption algorithm"""
-    for d in range(3, n, 2):
-        if d * e % n == 1:
-            return d
+    for i in range(3, totient, 2):
+        if i * public_exponent % totient == 1:
+            return i
 
 
 def encrypt(sequence, public_key):
     """encrypts a sequence with RSA encryption(c = m ** e % n) where m = message sequence (m, public_key)"""
-    n, e = public_key[0], public_key[1]
-    return sequence**e % n
+    modulus, public_exponent = public_key[0], public_key[1]
+    return sequence**public_exponent % modulus
 
 
 def decrypt(crypto_sequence, private_key):
     """decrypts a sequence with RSA encryption (m = c ** e % n) where c = encrypted sequence (c, private_key)"""
-    n, d = private_key[0], private_key[1]
-    return crypto_sequence**d % n
+    modulus, private_exponent = private_key[0], private_key[1]
+    return crypto_sequence**private_exponent % modulus
 
 
 def split_by_n(sequence, n):
-    """generator splits a sequence by n (sequence, n)"""
+    """generator splits a sequence by 'n' characters (sequence, n)"""
     while sequence:
         yield sequence[:n]
         sequence = sequence[n:]
 
 
-def split_encrypt(sequence, n):
-    """encrypts a sequence in n sized chunks and returns the list result (sequence, n)"""
-    sequence = list(split_by_n(sequence, n))
+def split_encrypt(sequence, split_size):
+    """encrypts a sequence of chunks and returns the result as a list (sequence, n)"""
+    sequence = list(split_by_n(sequence, split_size))
     encrypted_sequence = []
     for i, item in enumerate(sequence):
         encrypted_sequence.append(encrypt(int(sequence[i]), keys[0]))
     return encrypted_sequence
 
 
-def split_decrypt(encrypted_sequence, n):
-    """derypts a sequence in n sized chunks and returns the joined result (encrypted_sequence, n)"""
+def split_decrypt(encrypted_sequence):
+    """decrypts a chunked sequence and returns the joined result (encrypted_sequence)"""
     decrypted_sequence = []
     for i, item in enumerate(encrypted_sequence):
         decrypted_sequence.append(decrypt(int(encrypted_sequence[i]), keys[1]))
@@ -135,13 +146,12 @@ def split_decrypt(encrypted_sequence, n):
 if __name__ == '__main__':
 
     keys = generate_keys(1024, 2048)
-    #print keys
-    #keys = ((2249879, 61), (2249879, 368341))  #debug keys
+    # print keys
+    # keys = ((2249879, 61), (2249879, 368341))  #debug keys
     #keys = ((3233, 17), (3233, 2753))
 
     full_test()
-
-
-
-
-
+else:
+    # else statement to temporarily add debug keys for when calling kCrypt to test threading
+    #keys = ((2249879, 61), (2249879, 368341))
+    keys = ((3233, 17), (3233, 2753))
